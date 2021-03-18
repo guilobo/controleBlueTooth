2e787b38244f13e5837e4462cfbb8d10bbaa000c
Guilherme Lôbo <gui.wolflobo@gmail.com>
2021-03-18 16:02:15 -0300

    controle 2 finalizado

--- platforms/android/app/src/main/assets/www/js/custom.js
+++ platforms/android/app/src/main/assets/www/js/custom.js
@@ -56,3 +56,67 @@ $('#m1-seta-esquerda').click(function(){
 $('#m1-seta-direita').click(function(){
   bluetoothSerial.write('>', function(){}, function(){});
 });
+
+
+
+$('#m2-F').click(function(){
+  var estadoCaixaAtual = CaixaCaracterBotao(this);
+  if (estadoCaixaAtual == 'l')
+  bluetoothSerial.write('F', function(){}, function(){});
+  else
+  bluetoothSerial.write('f', function(){}, function(){});
+});
+$('#m2-G').click(function(){
+  var estadoCaixaAtual = CaixaCaracterBotao(this);
+  if (estadoCaixaAtual == 'l')
+  bluetoothSerial.write('G', function(){}, function(){});
+  else
+  bluetoothSerial.write('g', function(){}, function(){});
+});
+$('#m2-H').click(function(){
+  var estadoCaixaAtual = CaixaCaracterBotao(this);
+  if (estadoCaixaAtual == 'l')
+  bluetoothSerial.write('H', function(){}, function(){});
+  else
+  bluetoothSerial.write('h', function(){}, function(){});
+});
+$('#m2-I').click(function(){
+  var estadoCaixaAtual = CaixaCaracterBotao(this);
+  if (estadoCaixaAtual == 'l')
+  bluetoothSerial.write('I', function(){}, function(){});
+  else
+  bluetoothSerial.write('i', function(){}, function(){});
+});
+$('#m2-J').click(function(){
+  var estadoCaixaAtual = CaixaCaracterBotao(this);
+  if (estadoCaixaAtual == 'l')
+  bluetoothSerial.write('J', function(){}, function(){});
+  else
+  bluetoothSerial.write('j', function(){}, function(){});
+});
+
+$('#m2-baixo').on("touchstart",function(){
+  bluetoothSerial.write('-', function(){}, function(){});
+}).on("touchend",function(){
+  bluetoothSerial.write('.', function(){}, function(){});
+});
+$('#m2-cima').on("touchstart",function(){
+  bluetoothSerial.write('+', function(){}, function(){});
+}).on("touchend",function(){
+  bluetoothSerial.write('.', function(){}, function(){});
+});
+$('#m2-esquerda').on("touchstart",function(){
+  bluetoothSerial.write('/', function(){}, function(){});
+}).on("touchend",function(){
+  bluetoothSerial.write('=', function(){}, function(){});
+});
+$('#m2-direita').on("touchstart",function(){
+  bluetoothSerial.write('*', function(){}, function(){});
+}).on("touchend",function(){
+  bluetoothSerial.write('=', function(){}, function(){});
+});
+
+$("#m2Velocidade").change(function(){
+  var valorVelocidade = $(this).val();
+  bluetoothSerial.write(valorVelocidade, function(){}, function(){});
+})
