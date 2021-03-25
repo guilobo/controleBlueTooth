2a5b8665c088705cf34c8d7fe63f11347d6bee1b
Maicon Francisco <maicon.fr95@gmail.com>
2021-03-25 16:01:44 -0300

    Salva última aba utilizada

--- www/js/init.js
+++ www/js/init.js
@@ -1,5 +1,6 @@
   M.AutoInit();
   var modalDosBT = M.Modal.getInstance(document.querySelector("#menu-bt"));
+  var tabs = M.Tabs.getInstance(document.querySelector(".tabs"));
 
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
