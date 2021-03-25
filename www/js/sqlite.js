3e394075413d76d59d43e0483f22d5911161352e
Guilherme Lôbo <gui.wolflobo@gmail.com>
2021-03-25 15:01:56 -0300

    logica de criação de atualização do sqlite

---
+++ www/js/sqlite.js
@@ -0,0 +1,50 @@
+
+var db = null;
+document.addEventListener('deviceready', function() {
+  db = window.sqlitePlugin.openDatabase({name: 'conf.db', location: 'default'});
+
+  db.transaction(function(tx) {
+  tx.executeSql('CREATE TABLE IF NOT EXISTS config (id integer primary key, ultimaTab, ultimoDispositivo, ultimoDispositovoMac)');
+});
+});
+
+var ultimaTab;
+var ultimoDispositivo;
+var ultimoDispositovoMac;
+
+function CarregaDadosIniciais (){
+  db.executeSql('SELECT * FROM config order by id desc', [], function(rs) {
+     if(rs.rows.length == 0){
+       db.transaction(function(tx) {
+         tx.executeSql("INSERT into config (ultimaTab,ultimoDispositivo,ultimoDispositovoMac) VALUES ('controle1Tab','0','0')", []);
+       }, function(error) {
+       }, function() {
+         //Materialize.toast('UPDATE database OK', 3000, 'rounded red center-align');
+       });
+     }
+ var resultado = "";
+    ultimaTab = rs.rows.item(0).ultimaTab;
+    ultimoDispositivo = rs.rows.item(0).ultimoDispositivo;
+    ultimoDispositovoMac = rs.rows.item(0).ultimoDispositovoMac;
+    //abre a tab correta e devini o botão reconectar
+  });
+
+  }
+
+function AtualizaAUltimaAba(aba){
+  db.transaction(function(tx) {
+    tx.executeSql("UPDATE config set ultimaTab = '"+aba+"' where id = 1", []);
+  }, function(error) {
+  }, function() {
+    //Materialize.toast('UPDATE database OK', 3000, 'rounded red center-align');
+  });
+};
+
+function AtualizaOUltimoDispositivo(nome,mac){
+  db.transaction(function(tx) {
+    tx.executeSql("UPDATE config set ultimoDispositivo = '"+nome+"',ultimoDispositovoMac = '"+mac+"' where id = 1", []);
+  }, function(error) {
+  }, function() {
+    //Materialize.toast('UPDATE database OK', 3000, 'rounded red center-align');
+  });
+}
